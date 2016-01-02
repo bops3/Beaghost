96682bbb948f914995e21d9847da42bde9e8a9e4
ff17x3 <ff17x3@gmail.com>
2016-01-02 18:11:04 +0100

    commit!!

--- app/src/main/java/com/kritosoft/beaghost/GameManager.java
+++ app/src/main/java/com/kritosoft/beaghost/GameManager.java
@@ -1,7 +1,21 @@
 package com.kritosoft.beaghost;
 
+import java.util.Stack;
+
 /**
  * Created by Florian on 02.01.2016.
  */
 public class GameManager {
+
+    private Obstacle[] obstacles;
+    private Stack<Robot> robots;
+    private String mapFile = "map1.txt";
+
+    public GameManager() {
+
+    }
+
+    private void genMap() {
+
+    }
 }
---
+++ app/src/main/java/com/kritosoft/beaghost/Robot.java
@@ -0,0 +1,10 @@
+package com.kritosoft.beaghost;
+
+/**
+ * Created by Florian on 02.01.2016.
+ */
+public class Robot {
+    public Robot() {
+
+    }
+}
