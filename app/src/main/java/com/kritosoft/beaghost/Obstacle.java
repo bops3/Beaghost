56533bb6e13b2dd8a0a4399ce237aad660cf347b
ff17x3 <ff17x3@gmail.com>
2016-01-06 13:15:36 +0100

    retgf

--- app/src/main/java/com/kritosoft/beaghost/Obstacle.java
+++ app/src/main/java/com/kritosoft/beaghost/Obstacle.java
@@ -36,13 +36,15 @@ public class Obstacle implements Drawable {
         y = Float.parseFloat(sc.next());
         width = Float.parseFloat(sc.next());
         height = Float.parseFloat(sc.next());
-        Log.d("Obstacle","Obstacle erstellt: "+x+", "+y+" | "+width+", "+height);
+        Log.d("Obstacle", "Obstacle erstellt: " + x + ", " + y + " | " + width + ", " + height);
         return new Obstacle(x, y, width, height, gm);
     }
 
     @Override
     public void draw(Canvas c) {
-        Log.v("Obstacle", "drawing obstacle" +( x - gm.getOffsetX())+", "+(y - gm.getOffsetY()));
-        c.drawRect(x - gm.getOffsetX(), y - gm.getOffsetY(), width, height, paint);
+        Log.v("Obstacle", "drawing obstacle" + (x - gm.getOffsetX()) + ", " + (y - gm.getOffsetY()));
+        float drawX = x - gm.getOffsetX();
+        float drawY = y - gm.getOffsetY();
+        c.drawRect(drawX, drawY, drawX + width, drawY + height, paint);
     }
 }
