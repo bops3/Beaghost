5dce78ce9be4ebe101b35f5b79ec9e1b0e73b7da
ff17x3 <ff17x3@gmail.com>
2016-01-06 18:51:14 +0100

    von flo

--- app/src/main/java/com/kritosoft/beaghost/Robot.java
+++ app/src/main/java/com/kritosoft/beaghost/Robot.java
@@ -65,12 +65,12 @@ public class Robot implements Drawable {
 //        pointerY = (y + SIZE * 3 * dirSin + gm.getOffsetY()) * scale;
 //
 //        c.drawLine(drawX, drawY, pointerX, pointerY, pointerPaint);
-        Log.d("Robot","draw");
+        Log.v("Robot","draw");
         c.drawCircle(x, y, SIZE, bodyPaint);
     }
 
     public void tick(long delayMillis) {
-        Log.d("Robot","tick");
+        Log.v("Robot","tick");
 
         millisToNextDirChange = System.nanoTime() - lastDirChangeMillis;
         // Richtungsänderung ändern?
