b12de86a6c8642002928c42142f52352bf2ec0fc
Ma <machrivi99@gmail.com>
2016-01-07 13:30:27 +0100

    Max

--- app/src/main/java/com/kritosoft/beaghost/GameManager.java
+++ app/src/main/java/com/kritosoft/beaghost/GameManager.java
@@ -154,7 +154,8 @@ public class GameManager {
 
     public boolean isFree(float xW, float yW, float radius) {
         for (Obstacle o: obstacles) {
-            if (xW + radius <= o.x && xW >= o.x + o.width + radius && yW + radius <= o.y && yW >= o.y + o.height)
+            if (!(xW + radius <= o.x && xW >= o.x + o.width + radius && yW + radius <= o.y && yW >= o.y + o.height))
+                Log.d("Collisiondetection", "Collision!");
                 return false;
         }
         return true;
